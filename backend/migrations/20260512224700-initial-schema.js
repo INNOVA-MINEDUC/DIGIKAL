4a3762a5fb2d2e5dfb3df0cda88e37e40abe3103
Sincal200 <sincaljulio200@gmail.com>
2026-05-12 17:02:53 -0600

    feat: initialize database schema with Sequelize migrations and add sample data files

--- backend/migrations/20260512224700-initial-schema.js
+++ backend/migrations/20260512224700-initial-schema.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
+export default {
   async up(queryInterface, Sequelize) {
     // 1. ROLES
     await queryInterface.createTable('roles', {
